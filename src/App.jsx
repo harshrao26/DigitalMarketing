4b1831b5492bd921496a5049b2d396df793771e5
harshrao26 <harshurao058@gmail.com>
2025-02-04 10:58:43 +0530

    Whatsapp Button Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Routing from "./Routing.jsx";
 import Footer from "./components/Footer.jsx";
+import Whatsapp from "./components/Whatsapp.jsx";
 
 function App() {
   return (
